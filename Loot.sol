3977f8b0ed12c9da951e0aab319fa4287197a8cc
HalfSuperNate <96854633+HalfSuperNate@users.noreply.github.com>
2023-10-01 21:40:17 -0500

    Update Loot.sol

--- Loot.sol
+++ Loot.sol
@@ -10,6 +10,9 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 /// @author developer's website 🐸 https://www.halfsupershop.com/ 🐸
 contract Loot is Admins {
     
+    mapping(uint256 => uint256) public availableLootBoxes;
+    mapping(uint256 => mapping(uint256 => bool)) public claimedLootID;
+
     /*Tokens this contract holds*/
     mapping(address => uint256) public erc20ContractBalance;
     mapping(address => mapping(uint256 => bool)) public erc721ContractToken;
@@ -58,16 +61,23 @@ contract Loot is Admins {
     */
 
     constructor() Admins(msg.sender) {
-        // Initialize empty data for ERC20, ERC721, and ERC1155 tokens
         initTokenBundles();
     }
 
-    // Function to create a Loot Box
+    /**
+     * @dev Function to create a Loot Box.
+     * @param _ERC20BundleID ID of the ERC20 bundle.
+     * @param _ERC721BundleID ID of the ERC721 bundle.
+     * @param _ERC1155BundleID ID of the ERC1155 bundle.
+     * @param _root Merkle Root for verifying the Loot Box claim.
+     * @param _qty Quantity of Loot Boxes available.
+     */
     function createLootBox(
         uint256 _ERC20BundleID,
         uint256 _ERC721BundleID,
         uint256 _ERC1155BundleID,
-        bytes32 _root
+        bytes32 _root,
+        uint256 _qty
     ) public onlyAdmins returns (uint256) {
         require(_ERC20BundleID > 0 || _ERC721BundleID > 0 || _ERC1155BundleID > 0, "Loot box must contain at least one type of token");
 
@@ -83,6 +93,9 @@ contract Loot is Admins {
         // Add the Loot Box to the array
         uint256 newLootBoxId = LootBoxID.length;
         LootBoxID.push(newLootBox);
+
+        // Set available quantity
+        availableLootBoxes[newLootBoxId] = _qty;
         
         // Emit an event to log the creation
         emit LootBoxCreated(newLootBoxId, msg.sender);
@@ -90,8 +103,12 @@ contract Loot is Admins {
         return newLootBoxId;
     }
 
-    // Function to add an ERC20 token
-    function addERC20Token(address _tokenAddress, uint256 _balance) public {
+    /**
+     * @dev Function to add ERC20 token(s) to the bundle array.
+     * @param _tokenAddress The contract of the ERC20 tokens.
+     * @param _balance The amount (in WEI) of ERC20 tokens for this bundle.
+     */
+    function addERC20Token(address _tokenAddress, uint256 _balance) public onlyAdmins {
         // Create a new ERC20Token instance
         ERC20Token memory newToken = ERC20Token({
             tokenAddress: _tokenAddress,
@@ -102,20 +119,28 @@ contract Loot is Admins {
         ERC20BundleID.push(newToken);
     }
 
-    // Function to add an ERC721 token
-    function addERC721Token(address _tokenAddress, uint256[] calldata _tokenIDs) public {
+    /**
+     * @dev Function to add ERC721 token(s) to the bundle array.
+     * @param _tokenAddress The contract of the ERC721 tokens.
+     * @param _tokenIDs The token IDs for this bundle.
+     */
+    function addERC721Token(address _tokenAddress, uint256[] calldata _tokenIDs) public onlyAdmins {
         // Create a new ERC721Token instance
         ERC721Token memory newToken = ERC721Token({
             tokenAddress: _tokenAddress,
             tokenIDs: _tokenIDs
         });
 
-        // Add the new bundle to the array
         ERC721BundleID.push(newToken);
     }
 
-    // Function to add an ERC20 token
-    function addERC1155Token(address _tokenAddress, uint256[] calldata _tokenIDs, uint256[] calldata _balances) public {
+    /**
+     * @dev Function to add ERC1155 token(s) to the bundle array.
+     * @param _tokenAddress The contract of the ERC1155 tokens.
+     * @param _tokenIDs The token IDs for this bundle.
+     * @param _balances The amount of each ID for this bundle.
+     */
+    function addERC1155Token(address _tokenAddress, uint256[] calldata _tokenIDs, uint256[] calldata _balances) public onlyAdmins {
         // Create a new ERC1155Token instance
         ERC1155Token memory newToken = ERC1155Token({
             tokenAddress: _tokenAddress,
@@ -127,7 +152,45 @@ contract Loot is Admins {
         ERC1155BundleID.push(newToken);
     }
 
+    /**
+     * @dev Returns array token data for a ERC721 bundle, or array token or balance data for a ERC1155 bundle.
+     * @param _bundleID The bundle ID to check.
+     * @param _T721F1155 Flag true if ERC721 or false if ERC1155.
+     * @param _TidFamount Flag true for ERC1155 token IDs or false for ERC1155 amounts for each token ID.
+     */
+    function getBundleArrayData(uint256 _bundleID, bool _T721F1155, bool _TidFamount) public view returns (uint256[] memory) {
+        if(_T721F1155){
+            return ERC721BundleID[_bundleID].tokenIDs;
+        }
+        else{
+            if(_TidFamount){
+                return ERC1155BundleID[_bundleID].tokenIDs;
+            }
+            else{
+                return ERC1155BundleID[_bundleID].balances;
+            }
+        }
+    }
+
+    /**
+     * @dev Verify if the Loot can be claimed.
+     * @param proof bytes32 array for proof.
+     * @param _LootID Unique loot ID.
+     * @param _LootBoxID Specified loot box ID.
+     */
+    function verifyClaim(bytes32[] memory proof, uint256 _LootID, uint256 _LootBoxID) public view returns (bool) {
+        if (proof.length != 0 && !LootBoxID[_LootBoxID].claimed) {
+            if (MerkleProof.verify(proof, LootBoxID[_LootBoxID].root, bytes32(_LootID))) {
+                return (true);
+            }
+        }
+        
+        return (false);
+    }
+
+    // Initialize empty data for ERC20, ERC721, and ERC1155 tokens then push an empty LootBox
     function initTokenBundles() internal {
+        require(LootBoxID.length == 0);
         ERC20Token memory newToken = ERC20Token({
             tokenAddress: address(0),
             balance: 0
@@ -149,18 +212,16 @@ contract Loot is Admins {
         });
 
         ERC1155BundleID.push(__newToken);
-    }
 
-    function verifyMerkleProof(bytes32[] memory proof, bytes32 root, uint256 _LootID) public pure returns (bool) {
-        // Implement Merkle proof verification logic here
+        LootBox memory newLootBox = LootBox({
+            erc20: newToken,
+            erc721: _newToken,
+            erc1155: __newToken,
+            root: 0x0000000000000000000000000000000000000000000000000000000000000000,
+            claimed: false
+        });
 
-        if (proof.length != 0) {
-            if (MerkleProof.verify(proof, root, bytes32(_LootID))) {
-                return (true);
-            }
-        }
-        
-        return (false);
+        LootBoxID.push(newLootBox);
     }
 
 }
